0962d1368d883ca270e6fdb21190441fe25c4909
User Name <user@email.com>
2025-02-25 22:51:14 -0500

    Feat: Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+import { NavLink } from "react-router-dom"
+
+function Navbar() {
+  return (
+    <div className="navbar">
+        <div className="navbarLogo">📬MailBox</div>
+        <div className="navbarMenu">
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/mailboxes'>Mailboxes</NavLink>
+            <NavLink to='/newmail'>New Mailbox</NavLink>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
